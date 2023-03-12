38a872e3bdf0927f182290c3a3425432d566d1e2
Sofiia <sofiamanager26@gmail.com>
2023-03-12 16:53:17 +0200

    Add comment

--- src/main/java/s/safontseva/JobInterviewSingleton.java
+++ src/main/java/s/safontseva/JobInterviewSingleton.java
@@ -2,6 +2,7 @@ package s.safontseva;
 
 public final class JobInterviewSingleton {
     private static final Object LOCK = new Object();
+    //так как instance не volatile есть возможность получить лишь частично инициализированный объект
     private static JobInterviewSingleton instance;
 
     private JobInterviewSingleton() {
